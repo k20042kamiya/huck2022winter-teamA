c2202e59752ffd895e6914a7cc42f231c3f75743
k２００４２kk <k20042kk@kamiyamiyakoyuunoMacBook-Air.local>
2022-03-15 12:25:14 +0900

    変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-<<<<<<< HEAD
+
 import Datatable from './Datatable';
-=======
-// import Nanka from './dokoka';
-// import './test.css'
 import { useState } from 'react'
 
->>>>>>> 257596dc7e5e109273af4cba7990e3f7414b314d
 function App() {
     const now = new Date();
     const timestamp = `${now.getHours()}:${now.getMinutes().toString().padStart(2, 0)}`;
@@ -13,7 +9,8 @@ function App() {
     const [route, setRoute] = useState('0');
     const [timeSearch, setTimeSearch] = useState(timestamp);
 
-    return (<>
+    return (
+    <>
         <header>時刻表</header>
         <select name="route" id="route" value={ route } onChange={ e => setRoute(e.target.value) }>
             <option value="0">岡崎行き(環状線)</option>
@@ -21,33 +18,18 @@ function App() {
             <option value="2">藤が丘行き(リニモ)</option>
         </select>
         <ul id="timetable_list">
-            {
-<<<<<<< HEAD
-                new Array(1).map((_, k) =>
-                    <Datatable
-                        key={ k }
-                        route={ document.querySelector('#route').value }
-                        start={ document.querySelector('#time_search').value }
-                        date={ new Date() }
-                    />
-                )
-=======
-                // new Array(1).map((_, k) =>
-                //     <Nanka
-                //         key={ k }
-                //         route={ route }
-                //         start={ timeSearch }
-                //         date={ now }
-                //     />
-                // )
->>>>>>> 257596dc7e5e109273af4cba7990e3f7414b314d
-            }
+            <Datatable
+                route={route}
+                start={timeSearch}
+                
+                />
         </ul>
+        {}
         <footer>
             <input type="time" name="time_search" id="time_search" value={ timeSearch } onChange={ e => setTimeSearch(e.target.value) } required />
             <button>設定</button>
         </footer>
-    </>);
-}
-
+    </>
+    )
+    }
 export default App;
