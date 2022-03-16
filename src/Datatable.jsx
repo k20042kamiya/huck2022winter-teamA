707b9ff4770e4a11dd0de5c784585ebc5ca4adf3
k２００４２kk <k20042kk@kamiyamiyakoyuunoMacBook-Air.local>
2022-03-16 10:51:35 +0900

    電車時刻表動作確認

--- src/Datatable.jsx
+++ src/Datatable.jsx
@@ -10,17 +10,16 @@ function Datatime(props) {
     const LinimotoF_h = require('./timetable/train_LinertoF_h.json');
     const LinimotoF_s = require('./timetable/train_LinertoF_s.json');
     const LinimotoF_w = require('./timetable/train_LinertoF_w.json');
-
     const splitTime = props.start.split(":");
     const hour = parseInt(splitTime[0], 10);
     const minute = parseInt(splitTime[1], 10);
     const date = parseInt(props.date.getDate() - 1, 10);
     const month = parseInt(props.date.getMonth() + 1, 10);
     const Daia = Daiadata[month][date];
-    const trainDaia_h = [AikantoK_h, AikantoO_h, LinimotoF_h]
-    console.log(trainDaia_h);
+    
+    // console.log(trainDaia_h);
     console.log(date)
-    console.log(month)
+    // console.log(serchDaia)
     
     const serchTime = (timetable, hour, minute) => {
         if (timetable[hour] === undefined) return;
@@ -34,31 +33,19 @@ function Datatime(props) {
     }
     console.log(serchTime(data[Daia], hour, minute))
     const bustime = serchTime(data[Daia], hour, minute).split(":")
-
-    // const train_check = (route) => {
-    //     for (const d in trainDaia_h) {
-    //         if (Daia < 3) {
-    //             if (Number(route) === trainDaia_h.indexOf(d)) {
-    //                 return serchTime(trainDaia_h.indexOf(d), hour, bustime[1] + 10)
-    //             }
-    //         } else {
-    //             return serchTime(trainDaia_h[route], hour, minute)
-    //         }
-    //     }
-    // }
-
     const train_check = (route) => {
-        for (let i = 0; i < trainDaia_h.length; i++) {
-            if (Daia < 3 && Number(route) === i) {
-                    return serchTime(trainDaia_h[i],Number(bustime[0]), Number(bustime[1]) + 10)
-            } else {
-                return "ok"
-                //  return serchTime(trainDaia_h[route], hour, minute)
-            }
+                if (route === "0") {
+                    return serchTime(AikantoK_h, hour, bustime[1] + 10)
+                }
+                else if (route === "0") {
+                    return serchTime(AikantoO_h, hour, bustime[1] + 10)
         }
+             else {
+                return serchTime(LinimotoF_h, hour,bustime[1] + 10)
+            }
         
     }
-
+    
     console.log(typeof(bustime[1]))
     if (Daia < 3) {
         return (
@@ -79,7 +66,7 @@ function Datatime(props) {
             <div>
                 <li>
                 本日は運休です
-                電車時刻表 {train_check(props.route)}
+                {/* 電車時刻表 {serchTime(trainDaia, hour, minute)} */}
             </li>
             </div>
        
